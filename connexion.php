0ccf226ce0c5794c5b4145aa932096c9d6fda8d6
Prisca00738 <rachidyprisca137@gmail.com>
2025-06-22 11:08:52 -0700

    Update connexion.

--- connexion.php
+++ connexion.php
@@ -1,15 +1,1 @@
-<?php
-// Fichier : connexion.php
-$host = 'localhost';
-$dbname = 'my_class_room';
-$user = 'root'; // change selon ton config
-$pass = '';     // change selon ton config
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connexion réussie";
-} catch (PDOException $e) {
-    die("Erreur de connexion à la base de données : " . $e->getMessage());
-}
-?>
